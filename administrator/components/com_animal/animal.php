57935cc45ef3fd2d8b56cb3fbbab2698c14b406c
takeshiwayne <wayne@bm-sms.com.tw>
2014-04-03 09:21:19 +0800

    added controller and installed component

---
+++ administrator/components/com_animal/animal.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Part of JoomlaHow project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+$controller = JControllerLegacy::getInstance('animal');
+$controller = execute(JFactory::getApplication()->input->get('task'));
+$controller->redirect();
---
+++ administrator/components/com_animal/controller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Part of JoomlaHow project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Class AnimalController
+ *
+ * @since 1.0
+ */
+class AnimalController extends JControllerLegacy
+	{
+		protected $default_view = 'items';
+	}
