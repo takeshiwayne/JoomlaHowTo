6f63df531846661acba9742241f1cd52eb48909f
takeshiwayne <wayne@bm-sms.com.tw>
2014-04-05 19:29:10 +0800

    added items model

--- administrator/components/com_animal/models/items.php
+++ administrator/components/com_animal/models/items.php
@@ -4,4 +4,23 @@
  *
  * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
- */ 
+ */
+
+/**
+ * Class AnimalModelItems
+ *
+ * @since 1.0
+ */
+class AnimalModelItems extends JModelList
+{
+	public function getListQuery()
+	{
+		$db = $this->getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('a.*')
+			  ->from('#__animal AS a');
+
+		return $query;
+	}
+}
