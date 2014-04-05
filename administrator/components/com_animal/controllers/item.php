735e04e38b8987e563aaa81b980a5c52665cf26e
takeshiwayne <wayne@bm-sms.com.tw>
2014-04-05 22:07:51 +0800

    added controller item

---
+++ administrator/components/com_animal/controllers/item.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Part of JoomlaHow project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+class AnimalControllerItem extends JControllerForm
+{
+	
+}
